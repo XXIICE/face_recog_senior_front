77ec4117a8242fb4ce208c56c7d2d0934bbc6541
Kanisorn Chaiwichachan <conanza200@hotmail.com>
2020-09-15 11:52:07 +0700

    Update identify_face

--- frame_capture.py
+++ frame_capture.py
@@ -1,11 +1,11 @@
 import cv2
-import httpx
+# import httpx
 import json
 # from PIL import Image
 
 
 def capture_frame():
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(1)
     frameNo = 0
 
     while True:
@@ -19,7 +19,7 @@ def capture_frame():
         # frame = Image.fromarray(frame)
 
         if hasFace:
-            # cv2.imwrite("data/images/" + str(frameNo) + ".png", frame)
+            cv2.imwrite("data/images/" + str(frameNo) + ".png", frame)
             print("[INFO] Face detected with frame number:", frameNo)
             # identify_face(open("data/images/" + str(frameNo) + ".png", 'rb').read())
             frameNo += 1
@@ -63,36 +63,38 @@ def draw_face_box():
     pass
 
 
-def identify_face(image):
-# def identify_face():
+# def identify_face(image):
+def identify_face():
     # ENDPOINT = "http://0.0.0.0:5001/"
     # response = httpx.post(url=ENDPOINT, data=open("data/images/sample/S__5144578.jpg"))
     # print(response.text)
     import requests
 
-    url = "http://localhost:5001/"
+    url = "http://localhost:8002/api/v1/face/identify"
 
-    # payload = open("data/images/140.png", 'rb').read()
-    payload = image
+    payload = {
+        'file': ('filename.png', open("data/images/0.png", 'rb').read(), 'multipart/form-data')
+    }
+    # payload = image
     headers = {
-        'Content-Type': 'application/octet-stream'
+        'Content-Type': 'multipart/form-data'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.post(url, files=payload)
 
     print(response.text.encode('utf8'))
 
 
 if __name__ == "__main__":
     # Load models
-    print("[INFO] loading model...")
-    net = cv2.dnn.readNetFromCaffe(
-        "data/models/deploy.prototxt.txt",
-        "data/models/res10_300x300_ssd_iter_140000.caffemodel")
+    # print("[INFO] loading model...")
+    # net = cv2.dnn.readNetFromCaffe(
+    #     "data/models/deploy.prototxt.txt",
+    #     "data/models/res10_300x300_ssd_iter_140000.caffemodel")
 
-    capture_frame()
+    # capture_frame()
 
-    # identify_face()
+    identify_face()
 
     # frameNo = 0
     # cv2.imwrite("data/images/" + str(frameNo) + ".png", cv2.imread("data/images/sample/S__5144578.jpg"))
